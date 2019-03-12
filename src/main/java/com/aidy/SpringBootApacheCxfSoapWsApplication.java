13f69c5442aa7c2a432fbe946bb1a8282a76cef4
Aidy <aidyszh@gmail.com>
2019-03-12 22:38:24 +0800

    create spring boot project

---
+++ src/main/java/com/aidy/SpringBootApacheCxfSoapWsApplication.java
@@ -0,0 +1,13 @@
+package com.aidy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApacheCxfSoapWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApacheCxfSoapWsApplication.class, args);
+	}
+
+}
